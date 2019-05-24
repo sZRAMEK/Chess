e240d3e40026c99f61d984a5e4b6341ef8df41aa
sZRAMEK <przemyslaw.szramka@gmail.com>
2019-05-24 18:23:57 +0200

    Signed-off-by: sZRAMEK <przemyslaw.szramka@gmail.com>

---
+++ Assets/Enums/FigureType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Interfaces
+{
+    public enum FigureType
+    {
+        Bishop=10,
+        Rock = 11,
+        King = 12,
+        Knight = 13,
+        Queen = 14,
+        Pawn = 15
+    }
+}
